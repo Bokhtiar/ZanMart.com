ac6fbfbf8856e40e105769a831999ca3a0cd2d91
bokhtiar <bokhtiartoshar1@gmail.com>
2024-08-22 17:26:43 +0600

    routing setup and layout setup and toastify globally declear

---
+++ src/components/layouts/footer/index.jsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return <>
+     <footer className="bg-gray-800 p-4 text-white text-center">
+      <p>&copy; {new Date().getFullYear()} My Next.js App. All rights reserved.</p>
+    </footer>
+    </>
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import { LayoutPageWrapper } from "@/components/layouts";
+import { ToastContainer } from 'react-toastify'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <LayoutPageWrapper>
+      <Component {...pageProps} />
+      <ToastContainer/>
+    </LayoutPageWrapper>
+  )
 }
