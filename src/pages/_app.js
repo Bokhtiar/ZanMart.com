9c072da92f9a9e659de02cf7598c2abcad87484e
getch-hasan <hasan15-3112@diu.edu.bd>
2025-06-01 15:36:28 +0600

    tostify fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -21,8 +21,19 @@ export default function App({ Component, pageProps }) {
       <LayoutPageWrapper>
         {getLayout(<Component {...pageProps} />)}
         </LayoutPageWrapper>
-        <ToastContainer  
-          />
+        <ToastContainer
+position="top-right"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick={false}
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light"
+// transition={Bounce}
+/>
       </MyProvider>
     </div>
   );
