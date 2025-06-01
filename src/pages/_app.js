4ca9e1a818f7d849b58fb6b5e93ea3fc5e4ae965
zanit-solution <zanltd.bd@gmail.com>
2025-06-01 15:39:42 +0600

    fix:solve toastify issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,37 +3,35 @@ import { LayoutPageWrapper } from "@/components/layouts";
 import { ToastContainer } from "react-toastify";
 import { Inter, Poppins } from "next/font/google";
 import { MyProvider } from "@/contex/ProductsContex";
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation'; 
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
 
 export default function App({ Component, pageProps }) {
-  const getLayout =
-    Component.getLayout ||
-    ((page) => page );
+  const getLayout = Component.getLayout || ((page) => page);
   return (
     <div className={poppins.className}>
       <MyProvider>
-      <LayoutPageWrapper>
-        {getLayout(<Component {...pageProps} />)}
+        <LayoutPageWrapper>
+          {getLayout(<Component {...pageProps} />)}
         </LayoutPageWrapper>
         <ToastContainer
-position="top-right"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick={false}
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="light"
-// transition={Bounce}
-/>
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+          // transition={Bounce}
+        />
       </MyProvider>
     </div>
   );
