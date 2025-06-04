9e094ae386842ccea5ae855cb89685b00d44f4f1
getch-hasan <hasan15-3112@diu.edu.bd>
2025-06-04 12:58:23 +0600

    fix build error

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,28 +12,28 @@ export default function Document() {
           rel="stylesheet"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta
-          name="title"
-          content="ZanMart - Online Shopping for Zan Products"
-        />
-        <title >ZanMart - Online Shopping for Zan Products</title>
+        <meta name="title" content="ZanMart - Online Shopping for Zan Products" />
         <meta
           name="description"
           content="Discover a wide range of Zan products at ZanMart.com. Shop online for the best deals and premium quality."
         />
-        <meta
-          property="og:title"
-          content="ZanMart - Online Shopping for Zan Products"
-        />
-        <meta
-          property="og:description"
-          content="Discover a wide range of ZAN products at ZANMart.com. Shop online for the best deals and premium quality."
-        />
-        <meta
-          property="og:image"
-          content="https://zanmart.com/images/logo.png"
-        />
+        <meta property="og:title" content="ZanMart - Online Shopping for Zan Products" />
+        <meta property="og:description" content="Discover a wide range of ZAN products at ZANMart.com. Shop online for the best deals and premium quality." />
+        <meta property="og:image" content="https://zanmart.com/images/logo.png" />
         <meta property="og:url" content="https://zanmart.com" />
+
+        {/* ✅ Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JY1P989DB3"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-JY1P989DB3');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
