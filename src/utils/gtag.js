6fd6c6610e739f2e6b53e031d27a285fdbf9c6aa
bokhtiar <bokhtiartoshar1@gmail.com>
2025-06-04 12:27:30 +0600

    goole anylytic implementation

--- src/utils/gtag.js
+++ src/utils/gtag.js
@@ -1,13 +1,13 @@
-export const GA_TRACKING_ID = 'G-JY1P989DB3'; // আপনার Google Analytics ID
+export const GA_TRACKING_ID = 'G-JY1P989DB3'; 
 
-// pageview ট্র্যাক করার ফাংশন
+
 export const pageview = (url) => {
   window.gtag('config', GA_TRACKING_ID, {
     page_path: url,
   });
 };
 
-// কাস্টম ইভেন্ট ট্র্যাক করার ফাংশন (প্রয়োজনে)
+
 export const event = ({ action, category, label, value }) => {
   window.gtag('event', action, {
     event_category: category,
